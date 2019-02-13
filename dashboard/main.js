b3a5231b0c220c239971f1f5a5029065bcbedecf
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2019-02-12 20:37:18 -0800

    Firebase code for extracting dashboard data done

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,22 +1,31 @@
-function retrieve_data() {
+function retrieve_data(data_to_retrieive) {
   var database = firebase.database();
   var userId = "uid" // will be initialized with UID later firebase.auth().currentUser.uid;
   
-  firebase.database().ref('/users/' + userId + '/graphs/' + "expenses").once('value').then(function(snapshot) {
-    
-    
-    var date = (snapshot.val() && snapshot.val().date);
-    
-    snapshot.once('value').then(function(snapshot_2)) {
-      
-      var value = (snapshot_2.val() && snapshot_2.val().value);
-
-      console.log(date + " " + value)
-    }
-    
-  });
   
-  return 0
+  date_list = []
+  value_list = []
+  
+  firebase.database().ref('/users/' + userId + '/graphs/' + data_to_retrieive).on('value', (snapshot) => {
+    
+    snapshot.forEach((child) => {
+      console.log(child.key, child.val()); 
+      date_list.push(child.key)
+      value_list.push(child.val())
+    });
+      
+  });
+
+  
+  // firebase.database().ref('/users/' + userId + '/graphs/' + "expenses").once('value').then(function(snapshot) {
+  // 
+  //     snapshot.forEach((child) => {
+  //       console.log(child.key, child.val()); 
+  //     });
+  // 
+  //   });
+  
+    return [date_list, value_list]
 }
 
 
