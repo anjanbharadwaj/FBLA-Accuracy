a23e1ec3249934178c8ee7cef6f9a3d8cd12fbcb
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2019-02-12 21:16:06 -0800

    Pushing firebase code

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,4 +1,71 @@
-function retrieve_data(data_to_retrieive) {
+class GraphData {
+  constructor(date, value) {
+    this.date = date;
+    this.value = value;
+  }
+}
+
+class Employee {
+  constructor(email, name, phone) {
+    this.email = email
+    this.name = name
+    this.phone = phone
+  }
+}
+
+class Ledger_Item {
+  constructor(name, date, credit, debit) {
+    this.name = name
+    this.date = date 
+    this.credit = credit  
+    this.debit = debit  
+  }
+}
+
+
+function retrieve_ledger() {
+  var database = firebase.database();
+  
+  var userId = "uid"
+  
+  ledger_list = []
+  
+  database.ref('/users/' + userId + '/table/ledger/').on('value', (snapshot) => {
+    
+    snapshot.forEach((child) => {
+      console.log(child.key)
+      console.log(child.val()['credit'])
+            
+      ledger_list.push(new Ledger_Item(child.val()['name'], child.val()['date'], child.val()['credit'], child.val()['debit']))
+    })
+    
+  });
+  
+  return ledger_list
+}
+
+function retrieve_employees() {
+  var database = firebase.database();
+  
+  var userId = "uid"
+  
+  employee_list = []
+  
+  database.ref('/users/' + userId + '/list/employees/').on('value', (snapshot) => {
+    
+    snapshot.forEach((child) => {
+      console.log(child.key)
+      console.log(child.val()['email'])
+            
+      employee_list.push(new Employee(child.val()['email'], child.val()['name'], child.val()['phone']))
+    })
+    
+  });
+  
+  return employee_list
+}
+
+function retrieve_graph_data(data_to_retrieive) {
   var database = firebase.database();
   var userId = "uid" // will be initialized with UID later firebase.auth().currentUser.uid;
   
@@ -15,17 +82,8 @@ function retrieve_data(data_to_retrieive) {
     });
       
   });
-
   
-  // firebase.database().ref('/users/' + userId + '/graphs/' + "expenses").once('value').then(function(snapshot) {
-  // 
-  //     snapshot.forEach((child) => {
-  //       console.log(child.key, child.val()); 
-  //     });
-  // 
-  //   });
-  
-    return [date_list, value_list]
+  return new GraphData(date_list, value_list)
 }
 
 
